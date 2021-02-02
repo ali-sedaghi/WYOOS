3a3b60d6722ff36e38d33320e5a85bc51c975e60
nikomadol <ali_sedaghi@ymail.com>
2021-02-03 03:13:06 +0330

    Add caller to keybord in kernel

--- kernel.cpp
+++ kernel.cpp
@@ -1,6 +1,7 @@
 #include "types.h"
 #include "gdt.h"
 #include "interrupts.h"
+#include "keyboard.h"
 
 // Declaring printf function beacaue we don't access to built-in libs
 void printf(char* str) {
@@ -15,8 +16,8 @@ void printf(char* str) {
 
             // Newline
             case '\n':
-                y++;
                 x = 0;
+                y++;
                 break;
 
             // Other chars
@@ -29,8 +30,8 @@ void printf(char* str) {
 
         // Check the cursor location
         if (x >= 80) {
-            y++;
             x = 0;
+            y++;
         }
         if (y >= 25) {
             for (y = 0; y < 25; y++)
@@ -55,11 +56,13 @@ extern "C" void callConstructors() {
 // extern tell g++ not to change the function name on object file
 extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*magicnumber*/) {
     printf("Hello World! Welcome to nikomadol OS.");
-    printf("Hey");
 
     GlobalDescriptorTable gdt;
 
-    InterruptManager interrupts(&gdt);
+    InterruptManager interrupts(0x20, &gdt);
+
+    KeyboardDriver keyboard(&interrupts);
+
     interrupts.Activate();
     
     while(1);
