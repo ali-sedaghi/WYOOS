df13301486a147685e0f8f5fdd26ad2c53bccf73
nikomadol <ali_sedaghi@ymail.com>
2021-02-03 03:15:32 +0330

    Remove implementation of port functions from cpp file

--- port.cpp
+++ port.cpp
@@ -19,13 +19,11 @@ Port8Bit::~Port8Bit() {
 }
 
 void Port8Bit::Write(uint8_t data) {
-    __asm__ volatile("outb %0, %1" : : "a" (data), "Nd" (portnumber));
+    Write8(portnumber, data);
 }
 
 uint8_t Port8Bit::Read() {
-    uint8_t result;
-    __asm__ volatile("inb %1, %0" : "=a" (result) : "Nd" (portnumber));
-    return result;
+    return Read8(portnumber);
 }
 
 
@@ -40,13 +38,11 @@ Port16Bit::~Port16Bit() {
 }
 
 void Port16Bit::Write(uint16_t data) {
-    __asm__ volatile("outw %0, %1" : : "a" (data), "Nd" (portnumber));
+    Write16(portnumber, data);
 }
 
 uint16_t Port16Bit::Read() {
-    uint16_t result;
-    __asm__ volatile("inw %1, %0" : "=a" (result) : "Nd" (portnumber));
-    return result;
+    return Read16(portnumber);
 }
 
 
@@ -61,13 +57,11 @@ Port32Bit::~Port32Bit() {
 }
 
 void Port32Bit::Write(uint32_t data) {
-    __asm__ volatile("outl %0, %1" : : "a" (data), "Nd" (portnumber));
+    Write32(portnumber, data);
 }
 
 uint32_t Port32Bit::Read() {
-    uint32_t result;
-    __asm__ volatile("inl %1, %0" : "=a" (result) : "Nd" (portnumber));
-    return result;
+    return Read32(portnumber);
 }
 
 
@@ -82,5 +76,5 @@ Port8BitSlow::~Port8BitSlow() {
 }
 
 void Port8BitSlow::Write(uint8_t data) {
-    __asm__ volatile("outb %0, %1\njmp 1f\n1: jmp 1f\n1:" : : "a" (data), "Nd" (portnumber));
+    Write8Slow(portnumber, data);
 }
